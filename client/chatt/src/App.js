d0706898f052d033c785d49d2f740bd605fe6157
HEMANTH <siddunani1405@gmail.com>
2025-06-01 21:48:00 +0530

    Update App.js

--- client/chatt/src/App.js
+++ client/chatt/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import io from "socket.io-client";
-import { useState } from "react";
+import React,{ useState,useEffect } from "react";
 import Chat from "./Chat";
+import hero from "./hero.mp4";
+import logo from "./log.png";
 
-const socket = io.connect("https://chat-room-production-990c.up.railway.app/");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
+  
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
@@ -18,30 +21,43 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="full">
+    <img style={{top:'-12px',left:'1200px',position:'absolute'}} src={logo} height={150} width={150}/>
+    <video class="video-bg" src={hero} autoPlay loop muted/>
       {!showChat ? (
+        <>
+        <h1>Want to <b style={{color:'blue'}}>connect</b> and communicate<br/> 
+        with your friends, family, team, or community?<br/> 
+        Simply share the same <b style={{color:'blue'}}>Room ID</b> to join together<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-it's that easy and seamless.</h1>
+        <div className="App">
         <div className="joinChatContainer">
           <h3>Join A Chat</h3>
           <input
             type="text"
-            placeholder="John..."
+            placeholder="   John..."
             onChange={(event) => {
               setUsername(event.target.value);
             }}
           />
           <input
             type="text"
-            placeholder="Room ID..."
+            placeholder="   Room ID..."
             onChange={(event) => {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
+          <div className="but">
+         <button onClick={joinRoom}>Join A Room</button>
+         </div>
+         </div>
+        </div></>
       ) : (
-        <Chat socket={socket} username={username} room={room} />
+        <div className="App">
+        <Chat socket={socket} username={username} room={room} /></div>
       )}
-    </div>
+      <p style={{left:'500px',position:'absolute'}}>@2025 chat_room.All rights reserved</p>
+      </div>
   );
 }
 
